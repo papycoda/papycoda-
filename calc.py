01c45ffeb46f294c599207d134f31f7b31da2354
ogunbanwo opeyemi <58431589+papycoda@users.noreply.github.com>
2020-03-26 14:22:15 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 radius = int(input ("please enter the radius of your circle:"))
-area = 3.1472* radius
+area = 3.1472* (radius*radius)
 
 print ("the radius of your circle is:", area)
 input("Please Press enter to exit ;)")
